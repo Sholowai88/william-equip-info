cb2d6b08e3c5d5d80f26a8800455a630f27b7168
Sholowai88 <ntwc.william@gmail.com>
2026-08-17 16:29:16 +0800

    Create middleware.js
    Signed-off-by: Sholowai88 <ntwc.william@gmail.com>

---
+++ middleware.js
@@ -0,0 +1,37 @@
+// middleware.js
+export function middleware(request) {
+  const USERNAME = process.env.AUTH_USERNAME || "drws";
+  const PASSWORD = process.env.AUTH_PASSWORD || "DRws24685868";
+  
+  const authHeader = request.headers.get('authorization');
+  
+  if (!authHeader) {
+    return new Response('Authentication required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"'
+      }
+    });
+  }
+  
+  try {
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = atob(base64Credentials);
+    const [username, password] = credentials.split(':');
+    
+    if (username === USERNAME && password === PASSWORD) {
+      return;
+    }
+  } catch (error) {}
+  
+  return new Response('Invalid credentials', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"'
+    }
+  });
+}
+
+export const config = {
+  matcher: '/:path*',
+};
